8b02bf8ea5fd36421b139cf68cf86b32e62df908
amnona <amnonim@gmail.com>
2017-02-23 21:28:59 -0800

    add filter_id

--- calour/cahelper.py
+++ calour/cahelper.py
@@ -11,39 +11,6 @@ from logging import getLogger
 logger = getLogger(__name__)
 
 
-def filter_feature_ids(exp, ids, negate=False, inplace=False):
-    '''Filter features based on a list of feature ids (index values)
-
-    Parameters
-    ----------
-    ids : iterable of str
-        the feature ids to filter
-    negate : bool (optional)
-        False (default) to keep only sequences matching the fasta file, True to remove sequences in the fasta file.
-    inplace : bool (optional)
-        False (default) to create a copy of the experiment, True to filter inplace
-
-    Returns
-    -------
-    newexp : Experiment
-        filtered so contains only sequence present in exp and in the fasta file
-    '''
-    logger.debug('filter_feature_ids for %d features input' % len(ids))
-    okpos = []
-    tot_ids = 0
-    for cid in ids:
-        tot_ids += 1
-        if cid in exp.feature_metadata.index:
-            pos = exp.feature_metadata.index.get_loc(cid)
-            okpos.append(pos)
-    logger.debug('loaded %d sequences. found %d sequences in experiment' % (tot_ids, len(okpos)))
-    if negate:
-        okpos = np.setdiff1d(np.arange(len(exp.feature_metadata.index)), okpos, assume_unique=True)
-
-    newexp = exp.reorder(okpos, axis=1, inplace=inplace)
-    return newexp
-
-
 def is_sample_v4(exp, region_seq='TACG', frac_have=0.4, min_reads=10):
     '''Test which samples in the experiment are not from the region.
     Based on the consensus sequence at the beginning of the region.
@@ -73,16 +40,3 @@ def is_sample_v4(exp, region_seq='TACG', frac_have=0.4, min_reads=10):
     ok_samples = np.where(frac_ok >= frac_have)[0]
     bad_samples = np.where(frac_ok < frac_have)[0]
     return list(newexp.sample_metadata.index[ok_samples]), list(newexp.sample_metadata.index[bad_samples])
-
-
-def set_log_level(level):
-    '''Set the debug level for calour
-
-    Parameters
-    ----------
-    level : int
-        10 for debug, 20 for info, 30 for warn, etc.
-    '''
-
-    clog = getLogger('calour')
-    clog.setLevel(level)
